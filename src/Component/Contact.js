1ef919129c0d7b8ae72731dc51101ad6617c83ee
Heshan Tharindu Kalubowila <55088720+heshant3@users.noreply.github.com>
2024-04-20 23:40:00 +0530

    contact form

--- src/Component/Contact.js
+++ src/Component/Contact.js
@@ -1,36 +1,203 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import emailjs from "@emailjs/browser";
 import "./Css/Contact.css";
-import { motion, useAnimation } from "framer-motion";
 
-export default function Contact() {
-  const controls = useAnimation();
+const ContactForm = () => {
+  const form = useRef();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [message, setMessage] = useState("");
+  const [focused, setFocused] = useState(false);
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    // Send email using EmailJS
+    emailjs
+      .sendForm("service_t4oo0lx", "template_2j5i5dd", form.current, {
+        publicKey: "oQw9kiCMbT9s_LE-1",
+      })
+      .then(
+        (result) => {
+          console.log("Email sent successfully:", result.text);
+          alert("Email sent successfully!");
+        },
+        (error) => {
+          console.error("Error sending email:", error.text);
+          alert("Error sending email!");
+        }
+      );
+
+    // Reset form fields
+    setName("");
+    setEmail("");
+    setPhone("");
+    setMessage("");
+    setFocused(false);
+  };
 
   useEffect(() => {
-    const handleScroll = () => {
-      const element = document.getElementById("contact");
-      const offset = window.innerHeight * 0.8; // Adjust this value according to when you want the animation to start
+    const inputs = document.querySelectorAll(".input");
 
-      if (element.getBoundingClientRect().top < offset) {
-        controls.start({
-          opacity: 1,
-          y: 0,
-        });
+    function focusFunc() {
+      let parent = this.parentNode;
+      parent.classList.add("focus");
+    }
+
+    function blurFunc() {
+      let parent = this.parentNode;
+      if (this.value === "") {
+        parent.classList.remove("focus");
       }
-    };
+    }
 
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [controls]);
+    inputs.forEach((input) => {
+      input.addEventListener("focus", focusFunc);
+      input.addEventListener("blur", blurFunc);
+
+      return () => {
+        input.removeEventListener("focus", focusFunc);
+        input.removeEventListener("blur", blurFunc);
+      };
+    });
+  }, []); // empty dependency array to ensure effect runs only once on mount
+
+  const handleFocus = () => {
+    setFocused(true);
+  };
+
+  const handleBlur = () => {
+    if (name === "" || email === "" || phone === "" || message === "") {
+      setFocused(false);
+    }
+  };
+
   return (
-    <div className="Contact-Container" id="contact">
-      <motion.h1
-        initial={{ y: 50, opacity: 0 }}
-        animate={controls}
-        transition={{ delay: 0.1, duration: 1, ease: "backInOut" }}
-        className="EmailText"
-      >
-        Info@heshan.dev
-      </motion.h1>
+    <div className="container" id="contact">
+      <span className="big-circle"></span>
+      <img src="img/shape.png" className="square" alt="" />
+      <div className="form">
+        <div className="contact-info">
+          <h3 className="title">Let's get in touch</h3>
+          <p className="text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+            dolorum adipisci recusandae praesentium dicta!
+          </p>
+
+          <div className="info">
+            <div className="information">
+              <i className="fas fa-map-marker-alt"></i> &nbsp; &nbsp;
+              <p>92 Cherry Drive Uniondale, NY 11553</p>
+            </div>
+            <div className="information">
+              <i className="fas fa-envelope"></i> &nbsp; &nbsp;
+              <p>lorem@ipsum.com</p>
+            </div>
+            <div className="information">
+              <i className="fas fa-phone"></i>&nbsp;&nbsp;
+              <p>123-456-789</p>
+            </div>
+          </div>
+
+          <div className="social-media">
+            <p>Connect with us :</p>
+            <div className="social-icons">
+              <a href="#">
+                <i className="fab fa-facebook-f"></i>
+              </a>
+              <a href="#">
+                <i className="fab fa-twitter"></i>
+              </a>
+              <a href="#">
+                <i className="fab fa-instagram"></i>
+              </a>
+              <a href="#">
+                <i className="fab fa-linkedin-in"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="contact-form">
+          <span className="circle one"></span>
+          <span className="circle two"></span>
+
+          <form onSubmit={handleFormSubmit} autoComplete="off">
+            <h3 className="title">Contact us</h3>
+            <div className="input-container">
+              <input
+                type="text"
+                name="name"
+                className="input"
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <label htmlFor="name" className={focused || name ? "active" : ""}>
+                Name
+              </label>
+              <span className={focused ? "focus" : ""}></span>
+            </div>
+            <div className="input-container">
+              <input
+                type="email"
+                name="email"
+                className="input"
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <label
+                htmlFor="email"
+                className={focused || email ? "active" : ""}
+              >
+                Email
+              </label>
+              <span className={focused ? "focus" : ""}></span>
+            </div>
+            <div className="input-container">
+              <input
+                type="tel"
+                name="phone"
+                className="input"
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+              />
+              <label
+                htmlFor="phone"
+                className={focused || phone ? "active" : ""}
+              >
+                Phone
+              </label>
+              <span className={focused ? "focus" : ""}></span>
+            </div>
+            <div className="input-container textarea">
+              <textarea
+                name="message"
+                className="input"
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              ></textarea>
+              <label
+                htmlFor="message"
+                className={focused || message ? "active" : ""}
+              >
+                Message
+              </label>
+              <span className={focused ? "focus" : ""}></span>
+            </div>
+            <input type="submit" value="Send" className="btn" />
+          </form>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default ContactForm;
--- src/Component/Galary.js
+++ src/Component/Galary.js
@@ -1,16 +1,140 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Css/Galary.css";
 import Image1 from "../assest/img-1.png";
+import Image2 from "../assest/img-2.png";
+import Image3 from "../assest/img-3.png";
+import Image4 from "../assest/img-4.png";
+import Image5 from "../assest/img-5.png";
+import Image6 from "../assest/img-7.png";
+import { motion, useAnimation } from "framer-motion";
 
 export default function Galary() {
+  const controls = useAnimation();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const element = document.getElementById("Galary");
+      const offset = window.innerHeight * 0.8; // Adjust this value according to when you want the animation to start
+
+      if (element.getBoundingClientRect().top < offset) {
+        controls.start({
+          opacity: 1,
+          y: 0,
+        });
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [controls]);
+
   return (
-    <div className="Galary-Container">
-      <h1 className="Galary-HeaderText">Discover</h1>
+    <div className="Galary-Container" id="Galary">
+      <motion.h1
+        initial={{ y: 50, opacity: 0 }}
+        animate={controls}
+        transition={{ delay: 0.1, duration: 1, ease: "backInOut" }}
+        className="Galary-HeaderText"
+      >
+        Discover
+      </motion.h1>
 
       <div className="Galary-BodyContainer">
-        <div className="GalaryBox1">
-          <img src={Image1} alt="" className="Image" />
-        </div>
+        <motion.div
+          initial={{ y: 50, opacity: 0 }}
+          animate={controls}
+          transition={{ delay: 0.2, duration: 1, ease: "backInOut" }}
+          className="itemBox one"
+        >
+          <motion.div
+            className="Center"
+            initial={{ y: 50, opacity: 0 }}
+            animate={controls}
+            transition={{ delay: 0.3, duration: 1, ease: "backInOut" }}
+          >
+            <img src={Image1} alt="" className="Image" />
+          </motion.div>
+        </motion.div>
+
+        <motion.div
+          initial={{ y: 50, opacity: 0 }}
+          animate={controls}
+          transition={{ delay: 0.4, duration: 1, ease: "backInOut" }}
+          className="itemBox two"
+        >
+          <motion.div
+            className="Center"
+            initial={{ y: 50, opacity: 0 }}
+            animate={controls}
+            transition={{ delay: 0.5, duration: 1, ease: "backInOut" }}
+          >
+            <img src={Image2} alt="" className="Image" />
+          </motion.div>
+        </motion.div>
+
+        <motion.div
+          initial={{ y: 50, opacity: 0 }}
+          animate={controls}
+          transition={{ delay: 0.6, duration: 1, ease: "backInOut" }}
+          className="itemBox three"
+        >
+          <motion.div
+            className="Center"
+            initial={{ y: 50, opacity: 0 }}
+            animate={controls}
+            transition={{ delay: 0.7, duration: 1, ease: "backInOut" }}
+          >
+            <img src={Image3} alt="" className="Image" />
+          </motion.div>
+        </motion.div>
+
+        <motion.div
+          initial={{ y: 50, opacity: 0 }}
+          animate={controls}
+          transition={{ delay: 0.8, duration: 1, ease: "backInOut" }}
+          className="itemBox four"
+        >
+          <motion.div
+            className="Center"
+            initial={{ y: 50, opacity: 0 }}
+            animate={controls}
+            transition={{ delay: 0.9, duration: 1, ease: "backInOut" }}
+          >
+            <img src={Image4} alt="" className="Image" />
+          </motion.div>
+        </motion.div>
+
+        <motion.div
+          initial={{ y: 50, opacity: 0 }}
+          animate={controls}
+          transition={{ delay: 1.0, duration: 1, ease: "backInOut" }}
+          className="itemBox five"
+        >
+          <motion.div
+            className="Center"
+            initial={{ y: 50, opacity: 0 }}
+            animate={controls}
+            transition={{ delay: 1.1, duration: 1, ease: "backInOut" }}
+          >
+            <img src={Image5} alt="" className="Image" />
+          </motion.div>
+        </motion.div>
+
+        <motion.div
+          initial={{ y: 50, opacity: 0 }}
+          animate={controls}
+          transition={{ delay: 1.2, duration: 1, ease: "backInOut" }}
+          className="itemBox six"
+        >
+          <motion.div
+            className="Center"
+            initial={{ y: 50, opacity: 0 }}
+            animate={controls}
+            transition={{ delay: 1.3, duration: 1, ease: "backInOut" }}
+          >
+            <img src={Image6} alt="" className="Image" />
+          </motion.div>
+        </motion.div>
       </div>
     </div>
   );
